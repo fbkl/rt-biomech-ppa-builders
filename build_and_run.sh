ffe454c1c1d245bb27e2e37d243a2832053f14c0
fbkl <frkle@hvl.no>
2025-10-17 13:01:53 +0200

    improved structure now mounts volumes properly

--- build_and_run.sh
+++ build_and_run.sh
@@ -2,14 +2,24 @@ set -e
 docker build -t debian-builder .
 export simbodyversion=3.7-1
 export catch2version=3.11.0-1
-export opensimversion=4.5.2-1
+export opensim_coreversion=4.5.2-1
 export spdlogversion=1.15.3-1
+
+somevols() {
+	local dep=$1
+	local var="${dep}version"
+	local ver=${!var}
+	printf -- "-v %s/src/%s:/src/%s-%s -v %s/debian_folders/%s:/src/%s-%s/debian" \
+		"$(pwd)" "$dep" "$dep" "$ver" \
+		"$(pwd)" "$dep" "$dep" "$ver"
+}
+
 docker run -it \
   -v $HOME/.ssh:/home/builder/.ssh:ro \
   -v $HOME/.gnupg:/home/builder/.gnupg:rw \
-  -v "$(pwd)/src/simbody":/src/simbody-${simbodyversion} \
-  -v "$(pwd)/src/catch2":/src/catch2-${catch2version} \
-  -v "$(pwd)/src/opensim-core":/src/opensim-${opensimversion} \
-  -v "$(pwd)/src/spdlog":/src/spdlog-${spdlogversion} \
+  $(somevols simbody) \
+  $(somevols catch2) \
+  $(somevols opensim_core) \
+  $(somevols spdlog) \
   debian-builder
 
